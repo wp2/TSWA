ddee6e6c6bbadd8036ad6d2216ef62f55632387d
Piotr <Piotr@DESKTOP-ANDQDDP>
2017-01-19 23:12:35 +0100

    Trochę tego jest. Zmieniono plik konfiguracyjny (pobierany rozmiar czcionki, kolor czcionki), sporo metod w LogicControllerze i w MainWindow.

--- TSWA/Logic/LogicController.cs
+++ TSWA/Logic/LogicController.cs
@@ -1,28 +1,93 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
+using System.Windows.Media;
 using System.Text;
-using System.Threading.Tasks;
 using System.Xml;
 using _ONP;
 
 namespace TSWA
 {
+    /* Struktura przechowujaca info o wyswietlanym tekscie: tresc, rozmiar czcionki, kolor czcionki */
+    public struct TextInformation {
+
+        public TextInformation(string a_Equation, LogicController.FontSizes a_FontSize, Brush a_Foreground) {
+            _equation = a_Equation;
+            _fontSize = (int)a_FontSize;
+            _foreground = a_Foreground;
+            
+        }
+
+        private string _equation;
+        private int _fontSize;
+        private Brush _foreground;
+
+        public string Equation {
+            get {
+                return _equation;
+            }
+        }
+
+        public int FontSize {
+            get {
+                return _fontSize;
+            }
+        }
+
+        public Brush Foreground {
+            get {
+                return _foreground;
+            }
+        }
+
+    }
+
+    /* Klasa umozliwiajaca przesylanie stringow jako argumenty eventow */
+    public class MyEventArgs : EventArgs {
+        public string MyEventString { get; set; }
+
+        public MyEventArgs(string myString) {
+            MyEventString = myString;
+        }
+    }
+
+    // Zarządza Logiką aplikacji 
     public class LogicController
     {
-        enum CurrentNumberBaseSystem { Binary, Octal, Decimal, Hexadecimal };
+        /* Event do aktualizacji wyswietlanego tekstu */
+        public event UpdateDisplayedText UpdateDisplay;
+        public EventArgs eUpdateArgs = null;
+        public delegate void UpdateDisplayedText(LogicController lc, EventArgs eUpdateArgs);
 
+        /* Event informujacy o bledzie */
+        public event DisplayErrorInfo ErrorOccurred;
+        public MyEventArgs eErrorArgs = null;
+        public delegate void DisplayErrorInfo(LogicController lc, MyEventArgs eErrorArgs);
+
+        static char[] SignsArray = { '+', '-', '*', '/', '%' };
+
+        /* Zestaw dostepnych systemow liczbowych i zmienna przechowujaca obecny system liczbowy */
+        enum NumberBaseSystem { Binary = 2, Octal = 8, Decimal= 10, Hexadecimal = 16 };
+        NumberBaseSystem CurrentNumberBaseSystem;
+
+        /* Zestaw dostepnych rozmiarow czcionek i zmienna przechowujaca obecny rozmiar czcionki */
+        public enum FontSizes { VerySmall = 15, Small = 25, Medium = 35, Big = 45, VeryBig = 50 };
+        FontSizes CurrentFontSize;
+
+        /* Obecny kolor czcionki */
+        Brush CurrentFontColor;
+
+        /* Obecne dzialanie matematyczne */
         string CurrentEquationState;
-        CurrentNumberBaseSystem m_CurrentNumberBaseSystem;
 
-
-        public LogicController() { Init(); }
         
-        // Zarządza Logiką aplikacji 
-        public void Init()
+        public LogicController() {
+            Init();
+        }
+        
+        private void Init()
         {
-            // Ustaw stany początkowe wpisane/ z pliku ?
+            // Ustaw stany początkowe wpisane z pliku
             FileStream FileInput = new FileStream("KalkulatorUstawienia.xml", FileMode.Open);
             XmlReader xml = XmlReader.Create(FileInput);
             while (xml.Read()) {
@@ -30,11 +95,19 @@ namespace TSWA
                     while (xml.Read()) {
                         if (xml.Name == "CurrentEquationState" && xml.NodeType != XmlNodeType.EndElement) {
                             xml.Read();
-                            this.CurrentEquationState = xml.Value;
+                            CurrentEquationState = xml.Value;
                         }
-                        if (xml.Name == "CurrentNumberBaseSystem" && xml.NodeType != XmlNodeType.EndElement) {
+                        else if (xml.Name == "CurrentNumberBaseSystem" && xml.NodeType != XmlNodeType.EndElement) {
                             xml.Read();
-                            this.m_CurrentNumberBaseSystem =  (CurrentNumberBaseSystem)Int32.Parse(xml.Value);
+                            CurrentNumberBaseSystem = (NumberBaseSystem)Int32.Parse(xml.Value);
+                        }
+                        else if (xml.Name == "CurrentFontSize" && xml.NodeType != XmlNodeType.EndElement) {
+                            xml.Read();
+                            CurrentFontSize = (FontSizes)Int32.Parse(xml.Value);
+                        }
+                        else if (xml.Name == "CurrentFontColor" && xml.NodeType != XmlNodeType.EndElement) {
+                            xml.Read();
+                            CurrentFontColor = new BrushConverter().ConvertFromString(xml.Value) as SolidColorBrush;  
                         }
 
                     }
@@ -42,52 +115,77 @@ namespace TSWA
             }
             xml.Close();
             FileInput.Close();
-
         }
 
-        public void ClearEquation()
+        /* Czysci dzialanie */
+        public void ClearEquation() {
+            CurrentEquationState = "0";
+            UpdateDisplay(this, eUpdateArgs);
+        }
+
+        /* Dodaje cyfre do dzialania */
+        public void AddNumberToEquation(string Number)
         {
-            this.CurrentEquationState = "0";
-
+            if(CurrentEquationState == "0") {
+                CurrentEquationState = Number;
+            }
+            else {
+                CurrentEquationState += Number;
+            }
+            UpdateDisplay(this, eUpdateArgs);
         }
 
-        public void OnNumberButtonClick(string Number)
+        /* Usuwa ostatni wprowadzony znak */
+        public void DeleteLastSignInEquation() {
+            if (CurrentEquationState != "0" && CurrentEquationState.Length > 1) {
+                CurrentEquationState = CurrentEquationState.Remove(CurrentEquationState.Length - 1);
+            }
+            else if (CurrentEquationState != "0" && CurrentEquationState.Length == 1) {
+                CurrentEquationState = "0";
+            }
+            UpdateDisplay(this, eUpdateArgs);
+        }
+
+        /* Dodaje znak do dzialania */
+        public void AddSignToEquation(char Operator)
         {
-            if(this.CurrentEquationState == "0") { ClearEquation(); }
-            this.CurrentEquationState += Number; 
+            bool bFound = false;
+            foreach(char sign in SignsArray) {
+                if (CurrentEquationState[CurrentEquationState.Length - 1] == sign) {
+                    CurrentEquationState = CurrentEquationState.Remove(CurrentEquationState.Length - 1, 1);
+                    CurrentEquationState += Operator;
+                    bFound = true;
+                    break;
+                }
+            }
+            
+            if (false == bFound) {
+                CurrentEquationState += Operator;
+            }
+
+            UpdateDisplay(this, eUpdateArgs);
         }
 
-        public void OnDelButtonClick() {
-
-        }
-
-        public void OnBasicOperatorClick(string Operator)
-        {
-            this.CurrentEquationState += Operator;
-        }
-
-        public void OnAdvOperatorClick(string Operator)
-        {
-            this.CurrentEquationState += Operator;
-        }
-
-        public void OnClearButtonClick()
-        {
-
-        }
-
-        public void OnBackspaceButtonClick()
-        {
-
-        }
-
-        public void OnEqualButtonClick()
-        {
-            Console.WriteLine("2 mod 2 = " + 2 % 2 + " expected = 1");
-            CurrentEquationState = "2%8 ";
+        /* Przekazuje dzialanie do obliczenia */
+        public void ExecuteCalculations() {
             ONP myONP = new ONP(CurrentEquationState);
             CurrentEquationState = myONP.ONPCalculationResult();
-            int t = 5;
+
+            /* W przypadku bledu we wprowadzonym dzialaniu wyslij eventa (GUI go odbiera i wyswietla) */
+            if(CurrentEquationState == "ERROR") {
+                ErrorOccurred(this, new MyEventArgs("Blad we wprowadzonym dzialaniu!"));
+                CurrentEquationState = "0";
+            }
+            UpdateDisplay(this, eUpdateArgs);
+        }
+
+        /* Wyslanie struktury z informacjami nt. wyswietlanego tekstu */
+        public TextInformation GetTextInformation() {
+            return new TextInformation(CurrentEquationState, CurrentFontSize, CurrentFontColor);
+        }
+
+        public void onWordButtonClick() {
+
         }
     }
 }
--- TSWA/MainWindow.xaml.cs
+++ TSWA/MainWindow.xaml.cs
@@ -19,55 +19,79 @@ namespace TSWA {
     /// </summary>
     public partial class MainWindow : Window
     {
+        /* LogicMaster odpowiada za logike kalkulatora */
         LogicController LogicMaster;
-        string m_strMessageBoxText;
 
+        /* Konstruktor kalkulatora */
         public MainWindow() {
             InitializeComponent();
             LogicMaster = new LogicController();
 
-            // LogicMaster.getDefaultSettings();
-            m_strMessageBoxText = "0"; // temporary
-            Displayer.Text = m_strMessageBoxText;
+            /* Wskazanie metody wywolywanej przy otrzymaniu eventa o koniecznosci pobrania nowej
+             * wartosci dzialania i wyswietlenia jej */
+            LogicMaster.UpdateDisplay += new LogicController.UpdateDisplayedText(UpdateDisplay);
+
+            /* Ponizsze wywolanie pobiera iniciujace, domyslne dane do wyswietlenia */
+            UpdateDisplay();
+
+            /* Wskazanie metody wywolywanej przy otrzymaniu eventa o bledzie, jego argumenty
+             * zawieraja dane do wyswietlenia w postaci stringa */
+            LogicMaster.ErrorOccurred += new LogicController.DisplayErrorInfo(DisplayMessageBox);
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e) {
-            e.Handled = true;
-            var myValue = ((Button)sender).Tag.ToString();
-            MessageBox.Show(String.Format("{0}, {1}, {2}, {3}", sender.ToString(),e.ToString(), e.OriginalSource.ToString(), myValue));
-            int rowNumber = (int)((Button)sender).GetValue(Grid.RowProperty);
-            int colNumber = (int)((Button)sender).GetValue(Grid.ColumnProperty);
-        }
-
+        /* Metoda wywolywana przy nacisnieciu przycisku numerycznego */
         private void Numeric_Button_Click(object sender, RoutedEventArgs e) {
-            e.Handled = true;
-            var myValue = ((Button)sender).Tag.ToString();
-
-            var currentlyDisplayed = Displayer.Text.ToString();
-            
-            if(string.Compare(currentlyDisplayed,"0") == 0) {
-                currentlyDisplayed = myValue;
-            }
-            else {
-                currentlyDisplayed = string.Concat(currentlyDisplayed, myValue);
-            }
-            Displayer.Text = currentlyDisplayed;
+            LogicMaster.AddNumberToEquation(((Button)sender).Tag.ToString());
         }
 
+        /* Metoda wywolywana przy nacisnieciu przycisku - operacji matematycznej */
+        private void Sign_Button_Click(object sender, RoutedEventArgs e) {
+            LogicMaster.AddSignToEquation(((Button)sender).Tag.ToString().ToCharArray()[0]);
+        }
+
+        /* Metoda wywolywana przy nacisnieciu przycisku kasujacego ostatni wprowadzony znak */
         private void Del_Button_Click(object sender, RoutedEventArgs e) {
-            
+            LogicMaster.DeleteLastSignInEquation();
         }
 
+        /* Metoda wywolywana przy nacisnieciu przycisku kasujacego ostatni wprowadzony znak */
         private void Clear_Button_Click(object sender, RoutedEventArgs e) {
             LogicMaster.ClearEquation();
         }
 
         private void Word_Button_Click(object sender, RoutedEventArgs e) {
-
+            LogicMaster.onWordButtonClick();
         }
 
+        /* Metoda wywolywana przy nacisnieciu przycisku rownania */
         private void Equal_Button_Click(object sender, RoutedEventArgs e) {
-            LogicMaster.OnEqualButtonClick();
+            LogicMaster.ExecuteCalculations();
+        }
+
+        /* Metoda aktualizacujace wyswietlany tekst */
+        private void UpdateDisplay() {
+            TextInformation newTextInformation = LogicMaster.GetTextInformation();
+            Displayer.Text = newTextInformation.Equation;
+            Displayer.FontSize = newTextInformation.FontSize;
+            Displayer.Foreground = newTextInformation.Foreground;
+        }
+
+        /* Metoda wywolywana przy otrzymaniu eventa o aktualizacji wyswietlanego tekstu */
+        private void UpdateDisplay(LogicController lc, EventArgs e) {
+            UpdateDisplay();
+        }
+
+        /* Wyswietlenie komunikatu o bledzie  */
+        private void DisplayMessageBox(LogicController lc, MyEventArgs e) {
+            MessageBox.Show(e.ToString());
         }
     }
 }
+
+//private void Button_Click(object sender, RoutedEventArgs e) {
+//    e.Handled = true;
+//    var myValue = ((Button)sender).Tag.ToString();
+//    MessageBox.Show(String.Format("{0}, {1}, {2}, {3}", sender.ToString(),e.ToString(), e.OriginalSource.ToString(), myValue));
+//    int rowNumber = (int)((Button)sender).GetValue(Grid.RowProperty);
+//    int colNumber = (int)((Button)sender).GetValue(Grid.ColumnProperty);
+//}
